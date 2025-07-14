0be338412f98a5460d99d7777ef2fc7ed45fe7e5
Phu Duong <phuhoang.duong@nashtechglobal.com>
2025-07-14 16:25:20 +0700

    fixed absent table issue for Docker

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TYPE task_status AS ENUM ('Pending', 'In Progress', 'Completed');
+
+CREATE TABLE tasks (
+                       id SERIAL PRIMARY KEY,
+                       name TEXT NOT NULL,
+                       status task_status NOT NULL DEFAULT 'Pending',
+                       creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
